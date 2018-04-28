25837a64a87f1b69cae22a3262f614885d4322ff
merritt Webb <merrittwebb@merritts-MacBook-Pro.local>
2018-04-28 17:27:33 -0500

    2nd upload

--- Bamazoncustomer.js
+++ Bamazoncustomer.js
@@ -1,5 +1,10 @@
-var mysql = require("mysql2");
+
 var inquirer = require("inquirer");
+
+var mysql = require("mysql2");
+
+
+
 var Table = require("easy-table");
 
 var connection = mysql.createConnection({
@@ -19,45 +24,52 @@ if (err) throw err;
 // displays all of the items for sale
 function display(){
     connection.query("select * from products", function (err, result){
+
         if (err) throw err;
         console.log(Table.print(result));
+
         prompt ();
 })};
 
 function prompt(){
+
     inquirer
+
     .prompt([
             {
-            name: "whichId",
+            name: "whatId",
             type: "input",
-            message: "Enter the ID number of the product you would like to buy"
+            message: "Enter the ID of the thing you would like to buy"
             },
+
             {
             name:"quantity",
+
             type:"input",
-            message: "How many would like to purchase?" 
+
+            message: "How many would you like to purchase?" 
             }
         ])
         .then(function(answer){
          connection.query(  
-         "SELECT stock_quantity FROM products WHERE id =?", [answer.whichId], function (err, res) {
+         "SELECT stock_quantity FROM products WHERE id =?", [answer.whatId], function (err, res) {
         if (err) throw err;
-         const prodId = answer.whichId;
-                 const newQuantity = (res[0].stock_quantity - answer.quantity)
-                    update(newQuantity, prodId);
+         const itemId = answer.whatId;
+                 const newStock = (res[0].stock_quantity - answer.quantity)
+                    update(newStock, itemId);
                 });
             });
 
-        function update (newQuantity, prodID){
+        function update (newStock, prodID){
          connection.query(
          "UPDATE products SET stock_quantity =? WHERE item_id = ?", function(err, res){
       if (err) throw err;
      [
         {
-             stock_quantity: newQuantity
+             stock_quantity: newStock
                  },
              {
-            id: prodId
+            id: itemId
              }
      ],function(err,results){
        console.log(results)
